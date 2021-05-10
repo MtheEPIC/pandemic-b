f028c8105570a7c0f7a140aa03c244cca13bd7ca
MtheEPIC <mtheepic1423@protonmail.com>
2021-05-10 17:48:57 +0300

    treat

--- sources/Player.cpp
+++ sources/Player.cpp
@@ -2,13 +2,30 @@
 #include "Player.hpp"
 
 namespace pandemic {
-	Player::Player(Board &game_board, City init_city)
+	Player::Player(Board& game_board, City init_city) //: Board(game_board)
 	{
-		
+		this->game_board = &game_board;
+		this->curr_city = init_city;
+		this->cards_for_cure = CARDS_FOR_CURE;
 	}
-	Player::Player(Board &game_board, City init_city, int discover_cure_cards)
+	Player::Player(Board& game_board, City init_city, int discover_cure_cards)// : Board(game_board)
 	{
-		
+		this->game_board = &game_board;
+		this->curr_city = init_city;
+		this->cards_for_cure = discover_cure_cards;
+	}
+	M Player::get_pandemic_levels()
+	{
+		return this->game_board->cities_pandemic_level;
+	}
+	void Player::set_pandemic_levels(City city, int level)
+	{
+		if (level == -1)
+		{
+			this->game_board->cities_pandemic_level[city]--;
+			return;
+		}
+		this->game_board->cities_pandemic_level[city] = level;
 	}
 	Player::~Player()
 	{
@@ -26,12 +43,28 @@ namespace pandemic {
 	{
 		return *this;
 	}
-	Player Player::treat()
+	Player Player::treat(City city)
 	{
-		return *this;
-	}
-	Player Player::treat(City)
-	{
+		if(this->get_pandemic_levels()[city] == 0)
+		{
+			throw std::logic_error("cant treat a city if there is no disease");
+		}
+		if(this->curr_city != city)
+		{
+			if(this->role() != "FieldDoctor")
+			{
+				auto it = std::find(this->cards.begin(), this->cards.end(), city);
+				if (it == this->cards.end())
+				{
+					throw std::logic_error("only FieldDoctor can treat a disease in a city his not in without having the proper card");	
+				}
+				this->cards.erase(it);
+			}
+		}
+		// check if cure exists
+		// -------------
+		this->set_pandemic_levels(city, -1);
+		
 		return *this;
 	}
 	Player Player::fly_charter(City)
@@ -48,6 +81,10 @@ namespace pandemic {
 	}
 	Player Player::discover_cure(Color)
 	{
+		if(this->cards.size() == this->cards_for_cure)
+		{
+			throw std::logic_error("not enough card to discover a cure");
+		}
 		return *this;
 	}
 	std::string Player::role()
--- sources/Player.hpp
+++ sources/Player.hpp
@@ -4,20 +4,25 @@
 #include "Board.hpp"
 #include "City.hpp"
 #include "Color.hpp"
+#define CARDS_FOR_CURE 5
 
 namespace pandemic {
-	class Player
+	class Player : public Board
 	{
 	public:
+		Board* game_board;
+		City curr_city;
 		std::vector<City> cards;
+		int cards_for_cure;
 		Player(Board&, City);
 		Player(Board&, City, int);
+		M get_pandemic_levels();
+		void set_pandemic_levels(City city, int level);
 		~Player();
 		Player take_card(City);
 		Player drive(City);
 		Player fly_direct(City);
-		Player treat();
-		Player treat(City); // for Virologist
+		Player treat(City);
 		Player fly_charter(City);
 		Player build();
 		Player fly_shuttle(City);
