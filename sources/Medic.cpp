7e711e5d2645e83adbd46b5378104ae12d4c9d72
MtheEPIC <mtheepic1423@protonmail.com>
2021-05-11 19:07:15 +0300

    all medic funcs

--- sources/Medic.cpp
+++ sources/Medic.cpp
@@ -15,11 +15,40 @@ namespace pandemic {
 		{
 			throw std::logic_error("cant treat a city if there is no disease");
 		}
-		if(this->curr_city != city && !this->is_neighbour_city(city))
+		if(this->curr_city != city)
 		{
 			throw std::logic_error("can only treat a disease in the current and neighbour cities");	
 		}
 		this->set_pandemic_levels(city, 0);
+		return *this;
+	}
+	void Medic::auto_cure(City city)
+	{
+		if(this->is_cure(city)){
+			this->set_pandemic_levels(city, 0);
+		}
+	}
+	Player& Medic::drive(City dest_city){
+		Player::drive(dest_city);
+		this->auto_cure(dest_city);
+		return *this;
+	}
+
+	Player& Medic::fly_direct(City dest_city){
+		Player::fly_direct(dest_city);
+		this->auto_cure(dest_city);
+		return *this;
+	}
+
+	Player& Medic::fly_charter(City dest_city){
+		Player::fly_charter(dest_city);
+		this->auto_cure(dest_city);
+		return *this;
+	}
+
+	Player& Medic::fly_shuttle(City dest_city){
+		Player::fly_shuttle(dest_city);
+		this->auto_cure(dest_city);
 		return *this;
 	}
 	std::string Medic::role()
--- sources/Medic.hpp
+++ sources/Medic.hpp
@@ -4,10 +4,16 @@
 namespace pandemic {
 	class Medic : public Player
 	{
+	protected:
+		void auto_cure(City);
 	public:
 		Medic(Board& game_board, City init_city);
 		~Medic();
 		Player& treat(City);
+		Player& drive(City);
+		Player& fly_direct(City);
+		Player& fly_charter(City);
+		Player& fly_shuttle(City);
 		static std::string role();
 	};
 }
